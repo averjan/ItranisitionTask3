70493cc011d9601df80428eef854fd7d3eabc64f
Averjan <timophey.krut@gmail.com>
2021-02-10 23:36:33 +0300

    Class Program
    Generates HMAC
    
    Class Battle
    
    Reads user's step and compares with computer's

---
+++ task/Battle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace task
+{
+    static class Battle
+    {
+        static int GetUserStep(string[] items)
+        {
+            int inp;
+            do
+            {
+                Console.WriteLine("Availble moves");
+                for (var i = 0; i < items.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1} - {items[i]}");
+                }
+
+                Console.WriteLine("0 - exit");
+                Console.Write("Enter your move: ");
+            }
+            while ((!int.TryParse(Console.ReadLine(), out inp))
+                    || (inp < 0) || (inp > items.Length));
+            return inp;
+        }
+        public static void Game(string[] items, string compStep)
+        {
+            int mid, shift, indexCompStep, indexUserStep;
+            indexUserStep = GetUserStep(items);
+            if (indexUserStep == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine($"Your move: {items[indexUserStep - 1]}");
+            Console.WriteLine($"Computer move: {compStep}");
+            mid = items.Length / 2;
+            shift = indexUserStep - (mid + 1);
+            if (shift < 0)
+            {
+                shift += items.Length;
+            }
+
+            items = items.Skip(shift).Concat(items.Take(shift)).ToArray();
+            indexCompStep = Array.IndexOf(items, compStep);
+            if (indexCompStep < mid)
+            {
+                Console.WriteLine("You win!");
+            }
+            else if (indexCompStep == mid)
+            {
+                Console.WriteLine("Draw!");
+            }
+            else
+            {
+                Console.WriteLine("You lose!");
+            }
+        }
+
+    }
+}
---
+++ task/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Linq;
+
+namespace task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if ((args.Length < 3) || (args.Length % 2 == 0))
+            {
+                Console.WriteLine("Number of arguments must be more than 2 and odd. Example: 1 2 3 4 5");
+                return;
+            }
+
+            if (args.Length != args.Distinct().Count())
+            {
+                Console.WriteLine("Arguments must be unic. Example: a b c");
+                return;
+            }
+
+            var rng = RandomNumberGenerator.Create();
+            var key = new byte[16];
+            rng.GetBytes(key);
+            var compStep = args[RandomNumberGenerator.GetInt32(0, args.Length)];
+            var hmac = new HMACSHA256(key);
+            var userHMAC = hmac.ComputeHash(compStep.Select(c => (byte)c).ToArray());
+            Console.WriteLine("HMAC:");
+            Console.WriteLine(string.Join("", userHMAC
+                                    .Select(c => c.ToString("X"))
+                                    .ToArray()));
+            Battle.Game(args, compStep);
+            Console.WriteLine(string.Join("", key.Select(c => c.ToString("X"))
+                                                .ToArray()));
+        }
+    }
+}
